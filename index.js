45f8595bbed3b13757e40e75434e821436bed0c4
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-07-11 15:51:27 -0500

    Initial commit - port from Replit

---
+++ index.js
@@ -0,0 +1,35 @@
+// JavaScript Coding Challenge
+
+// 1. Create an object named 'person' using object literal syntax. The object should have properties 'name' and 'age', with your name and age as the values. Log the object to the console.
+
+// 2. Access the 'name' and 'age' properties of the 'person' object using dot notation and log the values.
+
+// 3. Modify the 'age' property of the 'person' object to a different number and log the updated object.
+
+// 4. Add a new property 'job' to the 'person' object with a value of your choice. Log the updated object.
+
+// 5. Delete the 'age' property from the 'person' object and log the updated object.
+
+// 6. Add a method 'greet' to the 'person' object that logs a greeting message using the 'name' property. Call the 'greet' method.
+
+// 7. Create an array named 'numbers' using array literal syntax with five numbers of your choice. Log the array to the console.
+
+// 8. Access the first and third elements of the 'numbers' array using their indices and log the values.
+
+// 9. Modify the second element of the 'numbers' array to a different number and log the updated array.
+
+// 10. Log the length of the 'numbers' array.
+
+// 11. Use a for loop to iterate over the 'numbers' array and log each element.
+
+// 12. Use a while loop to iterate over the 'numbers' array and log each element.
+
+// 13. Use a for...of loop to iterate over the 'numbers' array and log each element.
+
+// 14. Create a JSON string named 'json' representing the 'person' object. Log the JSON string.
+
+// 15. Parse the 'json' string into a JavaScript object and log the object.
+
+// 16. Stringify the 'person' object into a JSON string and log the string.
+
+// Note: Use 'console.log' to log the results. Good luck!
